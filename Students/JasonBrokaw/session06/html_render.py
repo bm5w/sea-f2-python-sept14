dbec9ea0972e4c5f2269f9d57516df0b1e4e3081
jbbrokaw <jbrokaw@gmail.com>
2014-09-27 12:10:51 -0700

    Slight refactor, still not sure addtabs() is a great idea

--- Students/JasonBrokaw/session06/html_render.py
+++ Students/JasonBrokaw/session06/html_render.py
@@ -3,7 +3,7 @@ class Element(object):
     indentation_level = 0
     tabstring = u"  "
 
-    def __init__(self, content=""):
+    def __init__(self, content=None):
         self.content = [content] if content else []
 
     def append(self, s):
@@ -13,11 +13,18 @@ class Element(object):
         tabbed_list = [level * Element.tabstring + line for line in line_list]
         return tabbed_list
 
-    def render(self, file_out, extra_ind=0):
+    def render(self, file_out):
         tag_open = "<" + self.tag_name + ">"
         tag_close = "</" + self.tag_name + ">"
-        tabbed_content = self.addtabs(self.content, 1)
-        line_list = [tag_open] + tabbed_content + [tag_close]
-        line_list = self.addtabs(line_list, self.indentation_level + extra_ind)
-        output_string = "\n".join(line_list)
+        if not self.content:
+            output_string = Element.tabstring * self.indentation_level + \
+                            tag_open + tag_close
+        elif len(self.content) == 1:
+            output_string = Element.tabstring * self.indentation_level + \
+                            tag_open + self.content[0] + tag_close
+        else:
+            tabbed_content = self.addtabs(self.content, 1)
+            line_list = [tag_open] + tabbed_content + [tag_close]
+            line_list = self.addtabs(line_list, self.indentation_level)
+            output_string = "\n".join(line_list)
         file_out.write(output_string)
