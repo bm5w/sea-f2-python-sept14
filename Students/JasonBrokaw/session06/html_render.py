44d5cd979e5eb0a61a43d18154723ce7cc0015e7
jbbrokaw <jbrokaw@gmail.com>
2014-09-27 12:43:04 -0700

    Implements Step 2, tags containing only a string appear on one line

--- Students/JasonBrokaw/session06/html_render.py
+++ Students/JasonBrokaw/session06/html_render.py
@@ -1,7 +1,8 @@
+from __future__ import unicode_literals
+
 class Element(object):
-    tag_name = u"html"
-    indentation_level = 0
-    tabstring = u"  "
+    tag_name = "GENERIC ELEMENT"
+    tabstring = "  "
 
     def __init__(self, content=None):
         self.content = [content] if content else []
@@ -9,22 +10,37 @@ class Element(object):
     def append(self, s):
         self.content.append(s)
 
-    def addtabs(self, line_list, level=0):
-        tabbed_list = [level * Element.tabstring + line for line in line_list]
+    def addtabs(self, line_list):
+        tabbed_list = [Element.tabstring + line for line in line_list]
         return tabbed_list
 
-    def render(self, file_out):
+    def render_list(self):
+        """Return a 'properly tabbed' list of strings for Element and its content"""
         tag_open = "<" + self.tag_name + ">"
         tag_close = "</" + self.tag_name + ">"
         if not self.content:
-            output_string = Element.tabstring * self.indentation_level + \
-                            tag_open + tag_close
-        elif len(self.content) == 1:
-            output_string = Element.tabstring * self.indentation_level + \
-                            tag_open + self.content[0] + tag_close
+            return [tag_open + tag_close]
+        if len(self.content) == 1 and isinstance(self.content[0], unicode):
+            return [tag_open + self.content[0] + tag_close]
         else:
-            tabbed_content = self.addtabs(self.content, 1)
-            line_list = [tag_open] + tabbed_content + [tag_close]
-            line_list = self.addtabs(line_list, self.indentation_level)
-            output_string = "\n".join(line_list)
+            line_list = [tag_open]
+            for item in self.content:
+                if isinstance(item, Element):
+                    line_list.extend(self.addtabs(item.render_list()))
+                else:
+                    line_list.append(Element.tabstring + item)
+            line_list.append(tag_close)
+            return line_list
+
+    def render(self, file_out):
+        output_string = "\n".join(self.render_list())
         file_out.write(output_string)
+
+class Html(Element):
+    tag_name = "html"
+
+class Body(Element):
+    tag_name = "body"
+
+class P(Element):
+    tag_name = "p"
