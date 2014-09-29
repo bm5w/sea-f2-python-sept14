7326ba7d16f4074320add2c51b3b43176977a961
Casey MacPhee <caseymacphee@gmail.com>
2014-09-29 14:07:36 -0700

    Working on the recursive ability of the render method...

--- Students/CaseyMacPhee/session06/html_render.py
+++ Students/CaseyMacPhee/session06/html_render.py
@@ -8,25 +8,38 @@ Python class example.
 # The start of it all:
 # Fill it all in here.
 class Element(object):
-   	tag = 'html'
-   	indent = '    '
+    tag = 'html'
+    indent = '\t'
 
-	def __init__(self, content = None):
+    def __init__(self, content = None):
+        if content is None:
+            self.content_list = []
+        else:
+            self.content_list = [content]
 
-		if content is None:
-			self.content_list = []
-		else:
+    def render(self, file_out, ind = ""):
+        """render the content to file object"""
 
-			self.content_list = [content]
+        file_out.write(self.indent + ind + "<" + self.tag + ">\n" + self.indent + ind)
 
-   	def render(self, file_out, ind = ""):
-   		"""render the content to file object"""
-   		file_out.write("\t" + "<" + self.tag + ">\n\t")
-   		file_out.write("\n" + "\t"+ "\t".join(self.content_list))
-   		file_out.write("\n" + "\t" + "</" + self.tag +">\n")
+        for i in range(len(self.content_list)):
+            try:
+                file_out.write(self.content_list[i] + ("\n\t" + self.indent + ind))
+            except:
+                self.content_list[i].render(file_out, "    ")
 
-   	def append(self, newcontent):
-   		""" add something within the html element"""
-   		self.content_list.append(newcontent)
+        file_out.write("\n" + self.indent + ind + "</" + self.tag +">\n")
+
+
+    def append(self, newcontent):
+        """ add something within the html element"""
+        self.content_list.append(newcontent)
+
+class Html(Element):
+    tag = 'html'
+class Body(Element):
+    tag = 'body'
+class P(Element):
+    tag = 'p'
 
 
