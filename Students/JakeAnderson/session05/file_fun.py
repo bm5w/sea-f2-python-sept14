f0f8a9a78424ed416b1076ac55ee8a021f59b3c1
Jakealope <Jakealope@gmail.com>
2014-09-30 14:10:31 -0700

    added the file_fun assignment with whitespace cleaning

--- Students/JakeAnderson/session05/file_fun.py
+++ Students/JakeAnderson/session05/file_fun.py
@@ -4,21 +4,23 @@ import io
 import string
 #I kind of went crazy with the naming scheme...
 message = """ This program strips off all that annoying whitespace. If you want to write a new file, plug that new file name in, otherwise I will just take all the whitespace out of the original file."""
+file_coming_in = "session05/white_space.txt"
+file_goin_out = "session05/new_white_spcae.txt"
 
-def time_to_trash_the_whitespaces(file_coming_in, file_goin_out):
+def trash_the_whitespaces(file_coming_in, file_goin_out):
     #In this one, I'm using a map funtion
     file_in = io.open(file_coming_in).readlines()
-    file_out = map(string.strip(), file_in)
+    file_out = map(string.strip, file_in)
     file_is_out = io.open(file_goin_out, 'w')
     for line in file_out:
-        file_out.write(line+"\n")
+        file_is_out.write(line+"\n")
 
 
 def trashing_with_a_list_comp(file_coming_in, file_goin_out):
     #I'm using a list comprehesion for this one
-     file_in = io.open(white_space.txt).readlines()
+     file_in = io.open(file_coming_in).readlines()
      file_out = [line.strip()+"\n" for line in file_in]
-     io.open(file_goin_out, 's').writelines(file_out)
+     io.open(file_goin_out, 'w').writelines(file_out)
 
 
 if __name__ == '__main__':
@@ -31,5 +33,5 @@ if __name__ == '__main__':
             file_goin_out = sys.argv[2]
         except IndexError:
             file_goin_out = file_coming_in
-    time_to_trash_the_whitespaces(file_coming_in,file_goin_out)
-    #trashing_with_a_list_comp(file_coming_in, file_goin_out)
+    trash_the_whitespaces(file_coming_in,file_goin_out)
+    trashing_with_a_list_comp(file_coming_in, file_goin_out)
