88e9f50b7b37bed01e178a74f1c1f1d36a52e078
James Warren <jwarren116@gmail.com>
2014-09-30 20:09:34 -0700

    Complete html_render.py Part 5

--- Students/JamesWarren/session06/html_render.py
+++ Students/JamesWarren/session06/html_render.py
@@ -4,30 +4,33 @@ class Element(object):
     tag = "html"
     ind = ""
     
-    def __init__(self, content=None, id=None, style=None):
+    def __init__(self, content=None, **kwargs):
         if content is None:
             self.content = []
         else:
             self.content = [content]
-        if style is None:
-            self.style = []
-        else:
-            self.style = [style]
+        self.attributes = kwargs
 
-    def append(self,append_content, style=None):
+
+    def append(self,append_content):
         """ append new content to element """
         self.content.append(append_content)
+
 
     def render(self, file_out, ind=""):
         """ render the content to the given file """
         cind = ind
-        file_out.write("\n%s<%s>" % (ind, self.tag))
+        if len(self.attributes) == 0:
+            file_out.write("\n%s<%s>" % (ind, self.tag))
+        else:
+            attributepair = self.attributes.items()
+            file_out.write("\n%s<%s %s=%s>" % (ind, self.tag, attributepair[0][0], attributepair[0][1]))
         ind += "    "
         for item in self.content:
             try:
                 item.render(file_out, ind)
             except AttributeError:
-                file_out.write(("\n"+ind).join(self.content))
+                file_out.write("\n"+ind+self.content[0])
         file_out.write("\n%s</%s>" % (cind, self.tag))
 
 class Html(Element):
@@ -44,8 +47,17 @@ class Head(Element):
 
 class OneLineTag(Element):
     def render(self, file_out, ind=""):
-        #ind += "    "
-        file_out.write("\n%s<%s> %s </%s>" % (ind, self.tag, self.content, self.tag))
+        file_out.write("\n%s<%s>%s</%s>" % (ind, self.tag, self.content[0], self.tag))
 
 class Title(OneLineTag):
     tag = "title"
+
+class SelfClosingTag(Element):
+    def render(self, file_out, ind=""):
+        file_out.write("\n%s<%s />" % (ind, self.tag))
+
+class Hr(SelfClosingTag):
+    tag = "hr"
+
+class Br(SelfClosingTag):
+    tag = "br"
