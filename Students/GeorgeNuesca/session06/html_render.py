23fdc56a9f155c3a146a7fddcaf4133f7d397f02
georgen <george.nuesca@gmail.com>
2014-10-04 14:28:42 -0700

    Completed HTML render

--- Students/GeorgeNuesca/session06/html_render.py
+++ Students/GeorgeNuesca/session06/html_render.py
@@ -1,31 +1,93 @@
 class Element(object):
 
+    tag = u'html'
 
-    tag = 'HTML'
-    indent = '    '
-    # content = ''
-
-    def __init__(self, content = None): #initializer
+    def __init__(self, content = None, **kwargs): #initializer
         if content is not None:  #Check for none
             self.content = [str(content)]  # Making a list to append (knowing append in future)
         else:
             self.content = []  #
+        self.styling = []
+        for elem in kwargs.keys():
+            self.styling.append(' ' + elem + '=' + '"' + kwargs[elem] + '"')
 
-    #def write(self):
-
-    def append(self, new_content):
+    def append(self, new_content, **kwargs):
         ''' Add some new content to the element '''
         self.content.append(new_content)  # Now you can append content using this method
 
-    def render(self, file_out, ind = ""):
+    def render(self, file_out, ind = u'', **kwargs):
         ''' render the contet to the given file like object '''
-        file_out.write(ind + "<"+ self.tag+">" + ind + self.indent)
-        file_out.write( "\n" + ind + self.indent).join(self.content) )
-        file_out.write("\n" + ind + "<"/+self.tag + ">")
+        if kwargs == []:
+            file_out.write(ind + u"<" + self.tag + u">" + u"\n")  # Places beginning tags
+        else:
+            file_out.write(ind + u"<" + self.tag + ' '.join(self.styling) + u">" + u"\n")
 
+        mind = (ind)
 
-#e = Element()
-#e.content
-#e.indent
-#e.Element['this is some content']
-#e.append('some additional content')
+        ind += '    '
+
+        for elem in self.content:
+            if isinstance(elem, basestring):
+                file_out.write(ind + elem  + u"\n")
+            else:
+                elem.render(file_out, ind)
+
+        file_out.write(mind + u"</" + self.tag + u">" + u"\n")# + u"\n")  #Places closing tags
+
+class Html(Element):
+    tag = u'html'
+
+    def render(self, file_out, ind = ''):
+        file_out.write(u'<!DOCTYPE html>\n')
+        Element.render(self, file_out, ind = '')
+
+class Body(Element):
+    tag = u'body'
+
+class P(Element):
+    tag = u'p'
+
+class Head(Element):
+    tag = u'head'
+
+class Title(Element):
+    tag = u'title'
+
+    def render(self, file_out, ind = u''):
+        file_out.write(ind + u"<" + self.tag + u">" +
+            u''.join(self.content) + u"</" + self.tag + u">" + u"\n")
+
+class Hr(Element):
+    tag = u'hr'
+    def render(self, file_out, ind = u''):
+        file_out.write(ind + u"<" + self.tag + " />" + u"\n")
+
+class Br(Element):
+    def render(self, file_out, ind = u''):
+        file_out.write(ind + u"<br />" + u"\n")
+
+class A(Element):
+    tag = u'a'
+
+    def __init__(self, *args):
+        self.styling = str(u' href = ' + args[0])
+        self.content = [args[1]]
+
+    def render(self, file_out, ind = u''):
+        file_out.write(ind + u"<" + self.tag + self.styling + u">" +
+            u''.join(self.content) + u"</" + self.tag + u">" + u"\n")
+
+class Ul(Element):
+    tag = u'ul'
+
+class Li(Element):
+    tag = u'li'
+
+class H(Title):
+    def __init__(self, *args):
+        self.tag = u'h' + str(args[0])
+        self.content = args[1]
+
+class Meta(Hr):
+    def __init__(self, **kwargs):
+        self.tag = u'meta ' + kwargs.keys()[0] + u'=' + u'"' + kwargs.values()[0] + u'"'
