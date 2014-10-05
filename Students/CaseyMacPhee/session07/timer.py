87563fd2ab1554ef98a907f86234b9ffac243464
Casey MacPhee <caseymacphee@gmail.com>
2014-10-05 11:23:38 -0700

    Fixed the timer and made a nice print format

--- Students/CaseyMacPhee/session07/timer.py
+++ Students/CaseyMacPhee/session07/timer.py
@@ -1,14 +1,14 @@
 
-import timeit
+import time
 class Timer(object):
 
 	def __init__(self, handle_error):
 		self.handle_error = handle_error
-		self.start = timeit.timeit()
 	def __enter__(self):
+		self.start = time.time()
 		return self
 	def __exit__(self, exc_type, exc_val, exc_tb):
-		self.end = timeit.timeit()
-		print self.start - self.end
+		self.end = time.time()
+		print "Took: {} seconds".format(self.end - self.start)
 		return self.handle_error
 
