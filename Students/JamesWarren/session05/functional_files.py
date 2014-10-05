ffba266743c29158aa17b550f218950b4758fb1c
James Warren <jwarren116@gmail.com>
2014-10-04 20:13:05 -0700

    Complete functional_files.py

--- Students/JamesWarren/session05/functional_files.py
+++ Students/JamesWarren/session05/functional_files.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import io
 import sys
+import string
 
 file_in = sys.argv[1]
 file_out = sys.argv[2]
@@ -17,16 +18,27 @@ conf = raw_input("Are you sure you want to overwrite %s? [y/n] > " % file_out)
 
 if conf.lower() == "y":
 
-    fi = io.open(file_in, mode='r')
-    source = fi.read()
-    fi.close()
+    source_in = io.open(file_in, mode='r').readlines()
+    file_out = io.open(file_out, mode='w')
 
-    #fo = io.open(file_out, mode='w')
+    ### Map() Method
+    #write_out = map(string.strip, source_in)
+    #for line in write_out:
+    #    file_out.write(line+'\n')
+    ###
+
+    ### List Comprehension Method
+    write_out = [line.strip()+'\n' for line in source_in]
+    file_out.writelines(write_out)
+    ###
+
+    print "** PROCESS COMPLETE **"
 
 elif conf.lower() == "n":
     print
     print "Exiting..."
+    print
     print "----------"
     sys.exit()
 else:
-    pass
+    print "Invalid entry. Now exiting..."
