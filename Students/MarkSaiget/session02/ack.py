d6a82bc62b9134283d40d17d15a6775fcd666382
Mark Saiget <bm5w@users.noreply.github.com>
2014-12-21 21:49:13 -0800

    fix code errors in __name__ == "__main__"

--- Students/MarkSaiget/session02/ack.py
+++ Students/MarkSaiget/session02/ack.py
@@ -11,17 +11,20 @@ def ack(m, n):
     else:
         return ack(m-1, ack(m, n-1))
 
-if __ack.py__ == '__main__':
+if __name__ == '__main__':
     """Test m and n values from 0 to 4, in ackermann function."""
     # Only run when module is run (ie from command line)
     from numpy import matrix
     output = matrix([[1, 2, 3, 4, 5],
         [2, 3, 4, 5, 6],
         [3, 5, 7, 9, 11],
-        [5, 13, 29, 61, 125],
-        [13, 65533, 2**65536-3, 2**2**65536-3, 2**2**2**65536-3]
+        [5, 13, 29, 61, 125]#,
+        #[13, 65533, 2**65536-3, 2**2**65536-3, 2**2**2**65536-3]
     ])
-    for m in range(4):
+    print 'made it here'
+
+    for m in range(3):
         for n in range(4):
-            assert ack(m, n) == output(m, n)
+            assert ack(m, n) == output[m, n]
+            print "ack({0}, {1})".format(m, n)
     print "All Tests Pass"
