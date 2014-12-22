7c1336ab68a4807c9da1c4a0666dfa663de28a6e
Mark Saiget <bm5w@users.noreply.github.com>
2014-12-21 22:21:24 -0800

    fix docstring according to PEP 257. Add shebang line to ensure proper python version is utilized.

--- Students/MarkSaiget/session02/ack.py
+++ Students/MarkSaiget/session02/ack.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python
+"""
+Perform Ackermann function.
+http://en.wikipedia.org/wiki/Ackermann_function
+A(m, n) =
+    n+1   if  m = 0
+    A(m-1, 1)  if  m > 0 and n = 0
+    A(m-1, A(m, n-1))  if m > 0 and n > 0
+Return none when inputs are less than zero.
+"""
+
+
 def ack(m, n):
     """Perform Ackermann function."""
-    # function is not defined for input values less than 0
-    # Check inputs are greater than zero.
     if m < 0 or n < 0:
         return None
     if m == 0:
@@ -12,19 +22,21 @@ def ack(m, n):
         return ack(m-1, ack(m, n-1))
 
 if __name__ == '__main__':
-    """Test m and n values from 0 to 4, in ackermann function."""
+    """Test m and n values from 0 to 4, given values in wikipedia table.
     # Only run when module is run (ie from command line)
+    Cannot test m = 4 values as they exceed recursion limit of python.
+    """
     from numpy import matrix
     output = matrix([[1, 2, 3, 4, 5],
-        [2, 3, 4, 5, 6],
-        [3, 5, 7, 9, 11],
-        [5, 13, 29, 61, 125]#,
-        #[13, 65533, 2**65536-3, 2**2**65536-3, 2**2**2**65536-3]
-    ])
-    print 'made it here'
-
+                     [2, 3, 4, 5, 6],
+                     [3, 5, 7, 9, 11],
+                     [5, 13, 29, 61, 125]   # ,
+                    # [13, 65533, 2**65536-3, 2**2**65536-3, 2**2**2**65536-3]
+                     ])
     for m in range(3):
         for n in range(4):
             assert ack(m, n) == output[m, n]
-            print "ack({0}, {1})".format(m, n)
+            # print "ack({0}, {1})".format(m, n)
+    assert ack(-1, 0) is None
+    assert ack(2, -1) is None
     print "All Tests Pass"
