e5a4253060c63e0133f5d3045fcb300846b72bc6
Mark Saiget <bm5w@users.noreply.github.com>
2014-12-21 22:56:19 -0800

    Add __name__ == __main__ block to series.py to test functions in file.

--- Students/MarkSaiget/session02/series.py
+++ Students/MarkSaiget/session02/series.py
@@ -44,3 +44,26 @@ def sum_series(n, a=0, b=1):
     else:
         return sum(n-2) + sum(n-1)
 print "lucas(8) = ", lucas(8)
+
+
+def __name__ == "__main__":
+    """Perform block of tests using assert statements to check functions."""
+    fib_values = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+    lucas_values = [2, 1, 3, 4, 7, 11, 18, 29, 47, 76, 123]
+
+    # Check fibonacci sequence values are as expected
+    for i in range(1, len(fib_values)+1):
+        assert fib_values(i) == fibonacci(i)
+
+    # Check lucas sequence values are as expected
+    for i in range(1, len(lucas_values)+1):
+        assert lucas_values(i) == lucas(i)
+
+    # Check sum_series function, test list using default (fibonacci) values
+    for i in range(len(fib_values)):
+        assert sum_series(i) == fib_values(i-1)
+
+    # Check sum_series function, test list using 2,1 (sum_series) values
+    for i in range(len(lucas_values)):
+        assert sum_series(i, 2, 1) == fib_values(i-1)
+    print "All tests pass"
