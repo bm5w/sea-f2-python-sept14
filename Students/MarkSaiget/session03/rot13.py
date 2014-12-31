9f497fa23e2ed09f2a7082d81a4b96669e3efc21
Mark Saiget <bm5w@users.noreply.github.com>
2014-12-30 23:39:22 -0800

    Added more robust tests.  Make assert statements == not is.

--- Students/MarkSaiget/session03/rot13.py
+++ Students/MarkSaiget/session03/rot13.py
@@ -12,7 +12,11 @@ def rot13(text):
     return text.translate(table)
 
 if __name__ == '__main__':
-        """Test rot13 function and helper functions work properly."""
-        test = "The ROT13 encryption scheme is a simple substitution cyper.!?"
-        print test
-        print rot13(test)
+    """Test rot13 function and helper functions work properly."""
+    test = "The ROT13 encryption scheme is a simple substitution cyper.?1!"
+    test_output = "Gur EBG13 rapelcgvba fpurzr vf n fvzcyr fhofgvghgvba plcre.?1!"
+    assert rot13(test) == test_output
+    test2 = "Rot13 or Rot-13 (short for rotate 13) is a simple letter substitution encryption scheme. It works by replacing the current english letters in a message with those that are 13 positions ahead in the alphabet. For example, the letter a is replaced by n, b by o, c by p, etc. Numbers and punctuation are not encoded."
+    test2_output = "Ebg13 be Ebg-13 (fubeg sbe ebgngr 13) vf n fvzcyr yrggre fhofgvghgvba rapelcgvba fpurzr. Vg jbexf ol ercynpvat gur pheerag ratyvfu yrggref va n zrffntr jvgu gubfr gung ner 13 cbfvgvbaf nurnq va gur nycunorg. Sbe rknzcyr, gur yrggre n vf ercynprq ol a, o ol b, p ol c, rgp. Ahzoref naq chapghngvba ner abg rapbqrq."
+    assert rot13(test2) == test2_output
+    print u"All tests pass."
