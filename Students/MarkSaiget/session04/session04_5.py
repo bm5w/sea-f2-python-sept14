4d2ef89c61e1a90354e912b965421b5e2eefe81d
Mark Saiget <bm5w@users.noreply.github.com>
2015-01-04 22:40:00 -0800

    add completed session04_5.py

---
+++ Students/MarkSaiget/session04/session04_5.py
@@ -0,0 +1,19 @@
+# Create a set with the letters in 'Python' and add 'i' to the set.
+string = "Python"
+pythonSet = set()
+for index in range(len(string)):
+    pythonSet.update([string[index]])
+pythonSet.add('i')
+print pythonSet
+
+# Create a frozenset with the letters in 'marathon'
+string = "marathon"
+marathon = []
+for index in range(len(string)):
+    marathon.append(string[index])
+fs = frozenset(string)
+print fs
+
+# display the union and intersection of the two sets.
+print "union of two sets: ", pythonSet.union(fs)
+print "intersection of two sets: ", pythonSet.intersection(fs)
