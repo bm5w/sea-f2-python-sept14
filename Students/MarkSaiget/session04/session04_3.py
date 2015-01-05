cfada63a50235f0d4502583a5bf79c971a2b1ecb
Mark Saiget <bm5w@users.noreply.github.com>
2015-01-04 22:19:04 -0800

    add session04_03.py

---
+++ Students/MarkSaiget/session04/session04_3.py
@@ -0,0 +1,10 @@
+"""Using the dictionary from item 1: Make a dictionary using the same keys but
+with the number of 'a's in each value."""
+
+# dict from item 1
+dict = {'name': 'Chris', 'city': 'Seattle', 'cake': 'Chocolate'}
+
+dict2 = {}
+for keys in dict.iterkeys():
+    dict2[keys] = dict[keys].count("a")
+print dict2
