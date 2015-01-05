43ea2db16f437fa844b6062dae3119f6cd402521
Mark Saiget <bm5w@users.noreply.github.com>
2015-01-04 21:06:01 -0800

    edit u4x2.py so that string formatting is used

--- Students/MarkSaiget/session04/u4x2.py
+++ Students/MarkSaiget/session04/u4x2.py
@@ -1,9 +1,6 @@
 """solution for session 04, warmup 2"""
 
 
-from decimal import Decimal
-
 inputted = (2, 123.4567, 10000)
-three = '%.0E' % Decimal(inputted[2])
-print "\"file_00{one}:{two}, {three}\"".\
-    format(one=inputted[0], two=round(inputted[1], 2), three=three)
+print "\"file_{one}:{two}, {three}\"".\
+    format(one='%.3d' % inputted[0], two='%.2f' % inputted[1], three='%.0E' % inputted[2])
