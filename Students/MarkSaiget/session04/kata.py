669d2a779a804c526363ee92ee16e4475e68c256
Mark Saiget <bm5w@users.noreply.github.com>
2015-01-06 22:00:26 -0800

    use setdefault dictionary method as more elegant solution than if/then

--- Students/MarkSaiget/session04/kata.py
+++ Students/MarkSaiget/session04/kata.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 """
 Implement trigram algorithm that generates a couple hundred words of text
 using  sherlock holmes (sherlock.txt) as input.
@@ -14,7 +15,7 @@ f = io.open('sherlock.txt', encoding='utf-8')
 header = f.read(header_size)
 sherlock = f.read()
 f.close
-text = sherlock[:10050].strip()
+text = sherlock.strip()
 text = text.replace('\n', ' ')          # remove \n values
 
 # Generate dictionary
@@ -27,11 +28,12 @@ while keepGoing:
     thirdSpace = text.find(u' ', secondSpace+1)
     key = text[start:secondSpace].strip()
     value = text[secondSpace+1:thirdSpace].strip()
-    # check if key exists already
-    if key in dict:
-        dict[key].append(value)
-    else:
-        dict[key] = [value]
+    # # check if key exists already
+    # if key in dict:
+    #     dict[key].append(value)
+    # else:
+    #     dict[key] = [value]
+    dict.setdefault(key, []).append(value)
     if text.find(u' ', thirdSpace) == -1:  # if no more spaces left, stop loop
         keepGoing = False
     start = firstSpace
@@ -39,12 +41,13 @@ while keepGoing:
 
 # Generate output
 output = ['Sherlock', 'Holmes']
+random.shuffle(dict['Sherlock Holmes'])
 output.append(dict['Sherlock Holmes'].pop())
 untilPeriod = True
 while untilPeriod:
     lookup = u" ".join(output[len(output)-2:])
     if lookup in dict:
-        random.shuffle(dict[lookup])
+        random.shuffle(dict[lookup])        # shuffle values to get random
         output.append(dict[lookup].pop())
     else:
         output.append(output.pop().strip() + ('.'))
