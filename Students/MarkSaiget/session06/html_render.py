c3b85a7eabc01d236e041b200d4cc64c9c7edf61
Mark Saiget <bm5w@users.noreply.github.com>
2015-01-07 23:04:33 -0800

    finished up to exercise 7

--- Students/MarkSaiget/session06/html_render.py
+++ Students/MarkSaiget/session06/html_render.py
@@ -1,23 +1,107 @@
+#! /usr/bin/env python
+"""do session 6 hw, HTML render"""
+
+# all future strings are u''  is assumed
 from __future__ import unicode_literals
 
-class Element():
+
+class Element(object):
     tag = 'html'
     indent = '    '
 
-    def __init__(self, content=None):
+    def __init__(self, content=None, **kwargs):
         if content is not None:
-            self.content = [str(content)]
+            self.content = [content]
         else:
             self.content = []
+        self.attributes = kwargs
 
     def append(self, new_input):
-        """add in to string"""
+        """add into string"""
         self.content.append(new_input)
 
     def render(self, file_out, ind=""):
-        file_out.write("{ind1}<{tag}>\n{ind1}{indent1}".
-                       format(ind1=ind, indent1=self.indent, tag=self.tag))
-        file_out.write("\n{ind}{indent}".format(ind=ind, indent=self.indent)
-                       .join(self.content))
+        file_out.write("\n{ind}<{tag}".format(ind=ind, tag=self.tag))
+        for key, value in self.attributes.items():
+            file_out.write(" {key}=\"{value}\"".format(key=key, value=value))
+        file_out.write(">")
+        for con in self.content:
+            try:
+                con.render(file_out, self.indent+ind)
+            except AttributeError:
+                file_out.write("\n{ind}{indent}{con}".
+                               format(ind=ind, indent=self.indent, con=con))
         file_out.write("\n{ind}</{tag}>".format(ind=ind, tag=self.tag))
+
+
+class Html(Element):
+    tag = 'html'
+
+    def render(self, file_out, ind=""):
+        Element.render(self, file_out, "")
+
+
+class Body(Element):
+    tag = 'body'
+
+
+class P(Body):
+    tag = 'p'
+
+
+class Head(Element):
+    tag = 'head'
+
+
+class OneLineTag(Element):
+    def render(self, file_out, ind=""):
+        file_out.write("\n{ind}<{tag}".format(ind=ind, tag=self.tag))
+        for key, value in self.attributes.items():
+            file_out.write(" {key}=\"{value}\"".format(key=key, value=value))
+        file_out.write(">")
+        for con in self.content:
+            try:
+                con.render(file_out)
+            except AttributeError:
+                file_out.write("{con}".format(con=con))
+        file_out.write("</{tag}>".format(ind=ind, tag=self.tag))
+
+
+class Title(OneLineTag):
+    tag = 'title'
+
+
+class SelfClosingTag(Element):
+    def render(self, file_out, ind=""):
+        file_out.write("\n{ind}<{tag}>".format(ind=ind, tag=self.tag))
+
+
+class Hr(SelfClosingTag):
+    tag = "hr /"
+
+
+class Br(SelfClosingTag):
+    tag = "br /"
+
+
+class A(OneLineTag):
+    def __init__(self, link, content):
+        OneLineTag.__init__(self, content, href=link)
+        # or
+        # self.content = "<a href=\"{link}\">{content}</a>".\
+        #     format(link=link, content=content)
+
+class Ul(Element):
+    tag = 'ul'
+
+
+class Li(Element):
+    tag = "li"
+
+
+class H(OneLineTag):
+    tag = 'h'
+    def __init__(self, num, content, **kwargs):
+        OneLineTag.__init__(self, content, **kwargs)
+        self.tag = self.tag + str(num)
 
